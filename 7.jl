d2023ad2719787630552c607894bc7660bb61b2c
Simonov-Ivan <114141987+Simonov-Ivan@users.noreply.github.com>
2022-12-26 02:54:15 +0300

    Add files via upload

--- 7.jl
+++ 7.jl
@@ -1,21 +1,21 @@
-using HorizonSideRobots
-
-size_left_right = 15
-size_top_bott = 15
-r=Robot("fields/7task.sit", animate=true)
-
-while isborder(r, HorizonSide(0)) == false
-    move!(r, HorizonSide(0))
-end
-
-function finding_exit(r)
-    while true
-        if isborder(r, HorizonSide(0)) == true
-            move!(r, HorizonSide(3))
-        else
-            break;
-        end
+function iceout!(robot,side)
+    side1 = side
+    side_for_move = right(side1)
+    n = 0
+    while isborder(robot,side1)
+        n+=1
+        along!(robot,side_for_move,n)
+        side_for_move = inverse(side_for_move)
     end
 end
 
-finding_exit(r)
+function along!(robot,side,n_steps)
+    for _ in 1:n_steps
+        move!(robot,side)
+    end
+end
+
+
+left(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 3 , 4)) 
+right(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 1 , 4)) 
+inverse(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 2 , 4))
--- 8.jl
+++ 8.jl
@@ -1,3 +1,26 @@
-#1. ДАНО: Робот находится в произвольной клетке ограниченного прямоугольного поля без 
-   #внутренних перегородок и маркеров.
-   #ЁРЕЗУЛЬТАТ: Робот — в исходном положении в центре прямого креста из маркеров, расставленных вплоть до внешней рамки.
+function find_marker!(robot) 
+    k=0
+    side=Nord
+    while (!ismarker(robot))
+      for _i in 1:2
+        along!(side,k,robot)
+        side=right(side)
+      end
+      k+=1
+    end
+  end
+  
+  function along!(side,num_steps,robot)
+    for _i in 1:num_steps
+      if (!ismarker(robot))
+        move!(robot,side)
+      end
+    end
+  end
+  
+  
+  function solve!(robot)
+  find_marker!(robot)
+  end
+  
+  right(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side)+1, 4))
